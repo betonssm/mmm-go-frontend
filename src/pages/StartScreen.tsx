68854b2e66209be55f2517cc0151e31ea9cb9b82
betonssm <betonssm@gmail.com>
2025-04-08 21:51:00 +0200

    обновлён стартовый экран: звук + glow + масштаб

--- src/pages/StartScreen.tsx
+++ src/pages/StartScreen.tsx
@@ -3,13 +3,20 @@ import React from "react";
 import "./StartScreen.css";
 import mavrodikImg from "../assets/mavrodik_clean.png";
 import buttonImg from "../assets/start_button.png";
+import coinSound from "../assets/coin.mp3";
 
 export default function StartScreen({ onStart }: { onStart: () => void }) {
+  const playSoundAndStart = () => {
+    const audio = new Audio(coinSound);
+    audio.play().catch(e => console.log("Audio play error:", e));
+    onStart();
+  };
+
   return (
     <div className="start-wrapper">
       <div className="start-image-container">
         <img src={mavrodikImg} alt="Мавродик" className="start-image" />
-        <button className="start-button" onClick={onStart}>
+        <button className="start-button glow" onClick={playSoundAndStart}>
           <img src={buttonImg} alt="Начать" />
         </button>
       </div>
