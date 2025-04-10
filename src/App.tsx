f710b50b59706a45feb192bd9d07407607a2d754
betonssm <betonssm@gmail.com>
2025-04-10 12:34:55 +0200

    🐞 Вернули стартовый экран и исправили маршруты

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import MMMGo from "./pages/MMMGo";
 import LevelPage from "./pages/LevelPage";
 import RankPage from "./pages/RankPage";
 import InvestorsPage from "./pages/InvestorsPage";
 import RatingPage from "./pages/RatingPage";
+import StartScreen from "./pages/StartScreen";
 
 export default function App() {
+  const [started, setStarted] = useState(false);
+
+  if (!started) {
+    return <StartScreen onStart={() => setStarted(true)} />;
+  }
+
   return (
     <Routes>
       <Route path="/" element={<MMMGo />} />
