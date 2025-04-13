819eee6e2e84b419b295c8f65ed0c03734890e58
betonssm <betonssm@gmail.com>
2025-04-13 14:25:00 +0200

    feat: отображение SR рейтинга на главной и в RatingPage

--- src/pages/MMMGo.tsx
+++ src/pages/MMMGo.tsx
@@ -315,7 +315,6 @@ useEffect(() => {
       >
         <h2>Привет, {playerName || "вкладчик"}!</h2>
         <p className="player-id">ID: {telegramId || "неизвестен"}</p>
-        <p className="ref-id">Реф: {new URLSearchParams(window.location.search).get("ref")}</p>
         <h1>
   Баланс:<br />
   {initialLoad || balance === null ? "Загрузка мавродиков..." : `${balance} мавродиков`}
