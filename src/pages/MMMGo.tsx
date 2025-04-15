6746e30c9484284fadf3e754aa0a514e8e183216
betonssm <betonssm@gmail.com>
2025-04-15 14:53:36 +0200

    🚫 Disable scroll on main page and enable it on others

--- src/pages/MMMGo.tsx
+++ src/pages/MMMGo.tsx
@@ -260,7 +260,6 @@ export default function MMMGo() {
 
   return (
     <>
-    <div className="telegram-frame-gradient"></div>
       {showLevelNotice && (
         <div className="level-up-notice">
           🎉 Новый уровень: {levelTitles[calculatedLevel]}!
