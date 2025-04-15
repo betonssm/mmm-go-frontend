9fc08682489667676c048a9466cc430d15ef2954
betonssm <betonssm@gmail.com>
2025-04-15 15:12:44 +0200

    🚫 Disable scroll on main page and enable it on others

--- src/pages/RankPage.tsx
+++ src/pages/RankPage.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../pages/MMMGo.css"; // общий стиль
-import bgRank from "../assets/bg-rank.png"; // путь к фону
+import bgRank from "../public/assets/bg-rank.png"; // путь к фону
 
 export default function RankPage() {
   const [bgLoaded, setBgLoaded] = useState(false);
