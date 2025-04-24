37a03c64c3c0b0fb937da47e274f26bb4923841d
betonssm <betonssm@gmail.com>
2025-04-24 22:19:31 +0200

    Удалено сообщение о незасчитанном реферале

--- src/pages/AdminLogs.tsx
+++ src/pages/AdminLogs.tsx
@@ -19,7 +19,7 @@ export default function AdminLogs() {
   }, []);
 
   return (
-    <DashboardLayout>
+   
     <div className="p-6 max-w-6xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Журнал действий</h1>
 
@@ -55,7 +55,7 @@ export default function AdminLogs() {
         </tbody>
       </table>
     </div>
-    </DashboardLayout>
+   
   );
 }
 
