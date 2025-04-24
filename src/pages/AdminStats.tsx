367bc74f9a8716a6835f0c981503b5b0899e3ea7
betonssm <betonssm@gmail.com>
2025-04-24 22:05:21 +0200

    Удалено сообщение о незасчитанном реферале

---
+++ src/pages/AdminStats.tsx
@@ -0,0 +1,15 @@
+
+// src/pages/AdminStats.tsx
+import React from "react";
+
+export default function AdminStats() {
+  return (
+    <div className="text-xl text-center mt-20 text-gray-600">
+      📈 Страница аналитики пока в разработке...
+    </div>
+  );
+}
+
+
+
+       
